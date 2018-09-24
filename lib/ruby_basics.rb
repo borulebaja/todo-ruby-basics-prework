df4a5e28bf0c8ff99e02052818155b3bcd6a13b3
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-24 17:41:00 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,5 +1,5 @@
 def division(num1, num2)
- 
+  return num1 / num2
 end
 
 def assign_variable(value)
@@ -15,11 +15,11 @@ def greeting
 end
 
 def return_a_value
-
+  
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase = "expert")
+  return phrase
 end
 
 def pizza_party
